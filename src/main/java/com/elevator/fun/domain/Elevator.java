c715ea0471d5d0b5cc1c2ba5f77f27dcd5cef52f
sharonmcdavis <sharonmcdavis@gmail.com>
2016-03-22 17:41:02 -0500

    Update Elevator.java

--- src/main/java/com/elevator/fun/domain/Elevator.java
+++ src/main/java/com/elevator/fun/domain/Elevator.java
@@ -17,20 +17,37 @@ public class Elevator {
 	PriorityQueue<Integer> destinationFloor; 
 	Integer currentFloor;
 	ElevatorStatus direction;
+	DoorStatus doorStatus;
 	
-	public void moveUp() {
+	public String moveUp() {
 		currentFloor++;
-		
+		return reportStatus();
 	}
 
-	public void moveDown() {
+	public String moveDown() {
 		currentFloor--;
-		
+		return reportStatus();
 	}
 
 	public void requestFloor(Integer newFloor) {
 		destinationFloor.add(newFloor);
 	}
 	
+	public String openDoor() {
+		doorStatus = DoorStatus.DOOR_OPEN;
+		return reportStatus();
+	}
 	
+	public String closeDoor() {
+		doorStatus = DoorStatus.DOOR_CLOSE;
+		return reportStatus();
+	}
+	public String reportStatus() {
+		return(
+		"requested floor count: " + destinationFloor.size() + 
+		"currentFloor: " + currentFloor + 
+		"direction: " + direction +
+		"door status: " + doorStatus
+		);
+	}
 }
